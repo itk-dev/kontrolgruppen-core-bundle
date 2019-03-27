4f8f1e13229ebdf89cc35161e15399cbb7c62a4a
Troels Ugilt Jensen <tujensen@gmail.com>
2019-03-27 09:42:40 +0100

    KON-2: Started bundle

---
+++ Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Kontrolgruppen\Core\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("", name="main")
+     */
+    public function index()
+    {
+        return $this->render('main/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test()
+    {
+        return $this->render('main/test.html.twig', []);
+    }
+}
