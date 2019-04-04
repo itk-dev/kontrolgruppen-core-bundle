141b94c5eabc0790d7f30c217313dcc3f27be9bc
Jesper Pedersen <jesper@yepz.dk>
2019-04-04 20:24:43 +0200

    KON-33: Add  copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -56,6 +57,9 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css"
-    })
+    }),
+    new CopyPlugin([
+      { from: 'Resources/public/', to: '../../../../../../public/bundles/kontrolgruppencore/', force: true }
+    ])
   ]
 };
