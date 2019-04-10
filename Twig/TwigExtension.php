dc296049992959e623b4636bbe7e9e0b80d33d24
Jesper Pedersen <jesper@yepz.dk>
2019-04-10 09:57:13 +0200

    KON-33: Merge develop

---
+++ Twig/TwigExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kontrolgruppen\CoreBundle\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class TwigExtension extends AbstractExtension
+{
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('iconClass', [$this, 'getIconClass']),
+        ];
+    }
+
+    public function getIconClass(string $name)
+    {
+        switch ($name) {
+            case 'dashboard': return 'fa-tachometer-alt';
+            case 'process': return 'fa-tasks';
+            case 'profile': return 'fa-id-card';
+            case 'users': return 'fa-users-cog';
+            case 'admin': return 'fa-cog';
+            case 'reminder': return 'fa-clock';
+
+            default: return '';
+        }
+    }
+}
