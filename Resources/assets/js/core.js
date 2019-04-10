cf4202085afd0318a3e5ab8fa527be351954ab17
Troels Ugilt Jensen <tujensen@gmail.com>
2019-04-10 15:22:41 +0200

    KON-3: Added coming reminders

--- Resources/assets/js/core.js
+++ Resources/assets/js/core.js
@@ -1,17 +1,8 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-const $ = require('jquery');
-// this "modifies" the jquery module: adding behavior to it
-// the bootstrap module doesn't export/return anything
+require('jquery');
 require('bootstrap');
-
-// any CSS you require will output into a single css file (app.css in this case)
 require('../css/core.scss');
+
+import 'whatwg-fetch';
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip(
@@ -19,4 +10,4 @@ $(function () {
       delay: {show: 400},
     }
   )
-})
+});
