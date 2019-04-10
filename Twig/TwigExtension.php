730de743c0731b244e9e16c60cd1cdb09c2500b2
Troels Ugilt Jensen <tujensen@gmail.com>
2019-04-10 10:30:16 +0200

    KON-6: Fixed coding standards

--- Twig/TwigExtension.php
+++ Twig/TwigExtension.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of aakb/kontrolgruppen-core-bundle.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
 namespace Kontrolgruppen\CoreBundle\Twig;
 
 use Twig\Extension\AbstractExtension;
@@ -17,13 +25,19 @@ class TwigExtension extends AbstractExtension
     public function getIconClass(string $name)
     {
         switch ($name) {
-            case 'dashboard': return 'fa-tachometer-alt';
-            case 'process': return 'fa-tasks';
-            case 'profile': return 'fa-id-card';
-            case 'users': return 'fa-users-cog';
-            case 'admin': return 'fa-cog';
+            case 'dashboard':
+                return 'fa-tachometer-alt';
+            case 'process':
+                return 'fa-tasks';
+            case 'profile':
+                return 'fa-id-card';
+            case 'users':
+                return 'fa-users-cog';
+            case 'admin':
+                return 'fa-cog';
 
-            default: return '';
+            default:
+                return '';
         }
     }
 }
