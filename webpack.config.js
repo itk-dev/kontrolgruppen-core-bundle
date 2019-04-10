f957fb5cb9435276149d2acc1bbb3467232c1ccb
Jesper Pedersen <jesper@yepz.dk>
2019-04-10 09:56:17 +0200

    KON-33: Remove copy assets function

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -57,9 +56,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css"
-    }),
-    new CopyPlugin([
-      { from: 'Resources/public/', to: '../../../../../../public/bundles/kontrolgruppencore/', force: true }
-    ])
+    })
   ]
 };
