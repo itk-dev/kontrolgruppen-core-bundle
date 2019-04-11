e530eea2aed72e13029fd3f2438d0f4c0cb933b2
Jesper Pedersen <jesper@yepz.dk>
2019-04-11 09:49:02 +0200

    KON-33: Add font-awesome from package

--- Resources/assets/js/core.js
+++ Resources/assets/js/core.js
@@ -10,6 +10,23 @@ const $ = require('jquery');
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
 
+// Add fontawesome
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faTachometerAlt } from "@fortawesome/free-solid-svg-icons/faTachometerAlt";
+import { faTasks } from "@fortawesome/free-solid-svg-icons/faTasks";
+import { faIdCard } from "@fortawesome/free-solid-svg-icons/faIdCard";
+import { faUsersCog } from "@fortawesome/free-solid-svg-icons/faUsersCog";
+import { faCog } from "@fortawesome/free-solid-svg-icons/faCog";
+import { faClock } from "@fortawesome/free-solid-svg-icons/faClock";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons/faUserPlus";
+import { faArchive } from "@fortawesome/free-solid-svg-icons/faArchive";
+import { faEye } from "@fortawesome/free-solid-svg-icons/faEye";
+import { faPencilAlt } from "@fortawesome/free-solid-svg-icons/faPencilAlt";
+import { faHouseDamage } from "@fortawesome/free-solid-svg-icons/faHouseDamage";
+
+library.add(faTachometerAlt, faTasks, faIdCard, faUsersCog, faCog, faClock, faUserPlus, faArchive, faEye, faPencilAlt, faHouseDamage);
+dom.watch();
+
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/core.scss');
 
