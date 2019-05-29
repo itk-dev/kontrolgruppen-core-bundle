a8954edc32592f89bb3f5e9a1922a4df5483445c
Troels Ugilt Jensen <tujensen@gmail.com>
2019-05-29 12:10:17 +0200

    KON-100: Fixed coding styles

--- Resources/assets/js/searchPage.js
+++ Resources/assets/js/searchPage.js
@@ -1,7 +1,7 @@
 import Mark from 'mark.js';
 
-$(document).ready(function() {
-  var context = document.querySelector(".js-search-result-rows");
-  var instance = new Mark(context);
-  instance.mark($('.js-search-text').data('search'));
+$(document).ready(function () {
+    var context = document.querySelector('.js-search-result-rows');
+    var instance = new Mark(context);
+    instance.mark($('.js-search-text').data('search'));
 });
