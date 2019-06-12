380e95c4779a22026c4042e5542e25c59dca5220
Mikkel Ricky <rimi@aarhus.dk>
2019-06-12 10:10:13 +0200

    KON-133: Added SAML logout handler

---
+++ Security/SAMLLogoutHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of aakb/kontrolgruppen-core-bundle.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
+namespace Kontrolgruppen\CoreBundle\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
+
+class SAMLLogoutHandler implements LogoutHandlerInterface
+{
+    /** @var \Kontrolgruppen\CoreBundle\Security\SAMLAuthenticator */
+    private $saml;
+
+    public function __construct(SAMLAuthenticator $saml)
+    {
+        $this->saml = $saml;
+    }
+
+    public function logout(
+        Request $request,
+        Response $response,
+        TokenInterface $token
+    ) {
+        $this->saml->getAuth()->logout();
+    }
+}
