1087567f9da5706d414422ddf88dc134495cd569
Lars Steen <lats@aarhus.dk>
2019-06-21 11:31:23 +0200

    KON-18: Adding extra data to log entry

--- EventListener/LoggableListener.php
+++ EventListener/LoggableListener.php
@@ -51,6 +51,46 @@ class LoggableListener extends BaseLoggableListener
         $om->flush();
     }
 
+    protected function prePersistLogEntry($logEntry, $object)
+    {
+        $data = $logEntry->getData();
+
+        if (!is_iterable($data)) {
+            return;
+        }
+
+        $newData = [];
+
+        foreach ($data as $key => $value) {
+
+            switch ($key) {
+
+                case 'channel':
+                    $newData[$key]['name'] = !is_null($object->getChannel()) ? $object->getChannel()->getName() : null;
+                    break;
+
+                case 'service':
+                    $newData[$key]['name'] = !is_null($object->getService()) ? $object->getService()->getName() : null;
+                    break;
+
+                case 'processType':
+                    $newData[$key]['name'] = !is_null($object->getProcessType()) ? $object->getProcessType()->getName() : null;
+                    break;
+
+                case 'processStatus':
+                    $newData[$key]['name'] = !is_null($object->getProcessStatus()) ? $object->getProcessStatus()->getName() : null;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        $data = array_merge_recursive($data, $newData);
+
+        $logEntry->setData($data);
+    }
+
     /**
      * {@inheritdoc}
      */
