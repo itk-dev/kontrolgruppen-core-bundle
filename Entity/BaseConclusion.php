b9ab32ef487b1af81d6a0946b7ddeaf3cb50f372
Lars Steen <lats@aarhus.dk>
2019-06-21 11:28:15 +0200

    KON-18: Adding loggable

--- Entity/BaseConclusion.php
+++ Entity/BaseConclusion.php
@@ -11,14 +11,17 @@
 namespace Kontrolgruppen\CoreBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity
+ * @Gedmo\Loggable()
  */
 class BaseConclusion extends Conclusion
 {
     /**
      * @ORM\Column(type="text", nullable=true)
+     * @Gedmo\Versioned()
      */
     private $conclusion;
 
