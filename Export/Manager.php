d1615a8497ba0e1cf6d345d85bb1044633d67dfa
Mikkel Ricky <rimi@aarhus.dk>
2019-07-08 15:39:26 +0200

    KON-125: Used services as exports

--- Export/Manager.php
+++ Export/Manager.php
@@ -10,11 +10,16 @@
 
 namespace Kontrolgruppen\CoreBundle\Export;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 /**
  * Class Manager.
  */
 class Manager
 {
+    /** @var \Symfony\Component\DependencyInjection\Container */
+    protected $container;
+
     /**
      * The configuration.
      *
@@ -25,8 +30,9 @@ class Manager
     /**
      * Constructor.
      */
-    public function __construct(array $configuration = [])
+    public function __construct(ContainerInterface $container, array $configuration = [])
     {
+        $this->container = $container;
         $this->configuration = $configuration;
     }
 
@@ -34,14 +40,21 @@ class Manager
     {
         $exports = [];
         if (isset($this->configuration['exports'])) {
-            foreach ($this->configuration['exports'] as $export) {
-                $class = $export['class'] ?? $export;
-                if (\is_string($class) && class_exists($class) && is_subclass_of($class, AbstractExport::class)) {
-                    $exports[$class] = new $class();
+            foreach ($this->configuration['exports'] as $service) {
+                $export = $this->getExport($service);
+                if (null !== $export) {
+                    $exports[\get_class($export)] = $export;
                 }
             }
         }
 
         return $exports;
+    }
+
+    public function getExport($service)
+    {
+        $export = $this->container->get($service);
+
+        return $export instanceof AbstractExport ? $export : null;
     }
 }
