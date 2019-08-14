141592f751d8792aebc33124986b5ab4768cf3aa
Lars Steen <lats@aarhus.dk>
2019-08-14 09:15:04 +0200

    KON-217: Applying coding standards

--- Resources/assets/js/processStatusChangeToggle.js
+++ Resources/assets/js/processStatusChangeToggle.js
@@ -4,16 +4,15 @@ $(document).ready(function () {
     if (searchParams.has('only_show_status')) {
         let onlyShowStatus = searchParams.get('only_show_status');
 
-        if ('true' == onlyShowStatus) {
-
-            $('#journal_filter_type').prop('disabled', function(i, v) { return !v });
+        if (onlyShowStatus === 'true') {
+            $('#journal_filter_type').prop('disabled', function (i, v) { return !v; });
             $('#onlyStatusCheckbox').attr('checked', true);
         }
     }
 
-    $('#onlyStatusCheckbox').on('change', function() {
-        if($(this).is(":checked")) {
-            $('#journal_filter_type').prop('disabled', function(i, v) { return !v });
+    $('#onlyStatusCheckbox').on('change', function () {
+        if ($(this).is(':checked')) {
+            $('#journal_filter_type').prop('disabled', function (i, v) { return !v; });
 
             let onlyStatus = $('#onlyStatusCheckbox').val();
 
