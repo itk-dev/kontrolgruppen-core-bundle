1b7aa13eac3be79c8eb35933463a5c55b352728a
Troels Ugilt Jensen <tujensen@gmail.com>
2020-01-30 14:25:28 +0100

    KON-321: Added comments

--- Entity/UserSettings.php
+++ Entity/UserSettings.php
@@ -40,16 +40,27 @@ class UserSettings
      */
     private $user;
 
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return string|null
+     */
     public function getSettingsKey(): ?string
     {
         return $this->settingsKey;
     }
 
+    /**
+     * @param string $settingsKey
+     *
+     * @return \Kontrolgruppen\CoreBundle\Entity\UserSettings
+     */
     public function setSettingsKey(string $settingsKey): self
     {
         $this->settingsKey = $settingsKey;
@@ -57,11 +68,19 @@ class UserSettings
         return $this;
     }
 
+    /**
+     * @return array|null
+     */
     public function getSettingsValue(): ?array
     {
         return $this->settingsValue;
     }
 
+    /**
+     * @param array $settingsValue
+     *
+     * @return \Kontrolgruppen\CoreBundle\Entity\UserSettings
+     */
     public function setSettingsValue(array $settingsValue): self
     {
         $this->settingsValue = $settingsValue;
@@ -69,11 +88,19 @@ class UserSettings
         return $this;
     }
 
+    /**
+     * @return \Kontrolgruppen\CoreBundle\Entity\User|null
+     */
     public function getUser(): ?User
     {
         return $this->user;
     }
 
+    /**
+     * @param \Kontrolgruppen\CoreBundle\Entity\User|null $user
+     *
+     * @return \Kontrolgruppen\CoreBundle\Entity\UserSettings
+     */
     public function setUser(?User $user): self
     {
         $this->user = $user;
