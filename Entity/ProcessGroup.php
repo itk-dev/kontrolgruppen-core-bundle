166dfdbe9eabbc009c942d9089e1168620dd1fa8
Lars Steen <lats@aarhus.dk>
2020-09-04 13:07:17 +0200

    KON-396: Adding logging for process groups

--- Entity/ProcessGroup.php
+++ Entity/ProcessGroup.php
@@ -13,9 +13,12 @@ namespace Kontrolgruppen\CoreBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass="Kontrolgruppen\CoreBundle\Repository\ProcessGroupRepository")
+ *
+ * @Gedmo\Loggable()
  */
 class ProcessGroup
 {
@@ -29,6 +32,8 @@ class ProcessGroup
     /**
      * @ORM\ManyToOne(targetEntity="Kontrolgruppen\CoreBundle\Entity\Process")
      * @ORM\JoinColumn(nullable=false)
+     *
+     * @Gedmo\Versioned()
      */
     private $primaryProcess;
 
@@ -39,6 +44,8 @@ class ProcessGroup
 
     /**
      * @ORM\Column(type="string", length=255)
+     *
+     * @Gedmo\Versioned()
      */
     private $name;
 
