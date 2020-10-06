84b60c63ce838dc1847260893c55e8143d767081
Lars Steen <lats@aarhus.dk>
2020-10-06 10:28:14 +0200

    KON-299: Adding Company form type

---
+++ Form/CompanyType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Kontrolgruppen\CoreBundle\Form;
+
+use Kontrolgruppen\CoreBundle\Entity\Company;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompanyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('CVR')
+            ->add('highlighted')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Company::class,
+        ]);
+    }
+}
