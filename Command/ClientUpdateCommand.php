14f0eb7efc0c0f6fc5e5cfc38f0e5f00fd0f94e7
Lars Steen <lats@aarhus.dk>
2020-12-01 10:35:06 +0100

    KON-412: Applying coding standards

--- Command/ClientUpdateCommand.php
+++ Command/ClientUpdateCommand.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of aakb/kontrolgruppen-core-bundle.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
 namespace Kontrolgruppen\CoreBundle\Command;
 
 use Doctrine\ORM\EntityManagerInterface;
@@ -14,7 +22,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Class ClientUpdateCommand
+ * Class ClientUpdateCommand.
  */
 class ClientUpdateCommand extends Command
 {
